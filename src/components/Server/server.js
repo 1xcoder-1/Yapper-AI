de1cb2f4f0b06e17af1aa9aed9f1440f8f4517a9
Abdullah <1xcoder@proton.me>
2025-12-22 16:01:04 +0500

    Update server.js

--- src/components/Server/server.js
+++ src/components/Server/server.js
@@ -4,14 +4,14 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-apiKey: "AIzaSyAMp7CPoG6skFFmXZm8QJ1NDkeqodSgR9g",
-  authDomain: "yapper-ai-fb253.firebaseapp.com",
-  projectId: "yapper-ai-fb253",
-  storageBucket: "yapper-ai-fb253.firebasestorage.app",
-  messagingSenderId: "1018492072723",
-  appId: "1:1018492072723:web:44f6841998fa0d48f4a0e6",
-  measurementId: "G-J2N4215QY5"
-};
+apiKey: "",
+   authDomain: "y",  
+  projectId: "",
+   storageBucket: "",
+   messagingSenderId: "",
+   appId: "",
+  measurementId: ""
+ };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
